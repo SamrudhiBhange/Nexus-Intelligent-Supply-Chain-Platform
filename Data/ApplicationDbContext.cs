9383e29ec9a7a375ac7cca7cebb2b54a2f2f4fce
Samrudhi <95043101+SamrudhiBhange@users.noreply.github.com>
2025-12-21 14:52:09 +0530

    Add files via upload
    Added

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Nexus.Services.Identity.Models;
+
+namespace Nexus.Services.Identity.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Add any custom configurations here later
+            // Add custom configurations here if needed
+            // Custom configurations
+            builder.Entity<ApplicationUser>(entity =>
+            {
+                entity.Property(e => e.FirstName).HasMaxLength(100);
+                entity.Property(e => e.LastName).HasMaxLength(100);
+            });
+        }
+    }
+}
+
